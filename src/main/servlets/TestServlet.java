801960113bc388f70578b91ab0093fd57009e21c
1604066yolo <karthikraman2011@gmail.com>
2018-01-30 20:00:59 -0500

    Test Commit 2

--- src/main/servlets/TestServlet.java
+++ src/main/servlets/TestServlet.java
@@ -1,5 +1,23 @@
 package main.servlets;
 
-public class TestServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		if (req.getAttribute("username").equals("user") && req.getAttribute("password").equals("pass"))
+			resp.sendRedirect("login.html");
+	}
 
 }
